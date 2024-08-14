4d01c1fb0ce606c0e0d014cdc67ac2815d489964
Vitor <vitorcostaofficial@gmail.com>
2024-08-14 10:11:02 -0300

    Acrescentei a opção de enviar para uma lista de email

--- App.java
+++ App.java
@@ -4,6 +4,8 @@ import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.SimpleEmail;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -12,8 +14,15 @@ public class App {
         String emailRemetente = JOptionPane.showInputDialog(null, "Digite seu email");
         String assuntoRemetente = JOptionPane.showInputDialog(null, "Digite o assunto do email");
         String mensagemRemetente = JOptionPane.showInputDialog(null, "Digite sua mensagem");
-        String emailDestinatario = JOptionPane.showInputDialog(null, "Digite o email para quem deseja enviar: ");
 
+        List<String> list = new ArrayList<>();
+
+        String quantidadeEmails = JOptionPane.showInputDialog(null, "Digite a quantidade de emails que deseja enviar: ");
+        for(int i = 0; i < Integer.parseInt(quantidadeEmails); i++){
+            String emailDestinatario = JOptionPane.showInputDialog(null, "Digite o email para quem deseja enviar: ");
+
+            list.add(emailDestinatario);
+        }
 
         JPasswordField jPasswordField = new JPasswordField();
         int option = JOptionPane.showConfirmDialog(null, jPasswordField, "Para finalizar, digite sua senha",
@@ -40,12 +49,16 @@ public class App {
                     email.setFrom(emailRemetente);
                     email.setSubject(assuntoRemetente);
                     email.setMsg(mensagemRemetente);
-                    email.addTo(emailDestinatario);
+
+                    for (String emailDestino : list) {
+                        email.addTo(emailDestino);
+                    }
+
                     email.send();
 
                     JOptionPane.showMessageDialog(null, "Email enviado com sucesso!");
                     JOptionPane.showMessageDialog(null, "Obrigado por usar nosso serviço " + nomeRemetente + " !");
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                     JOptionPane.showMessageDialog(null, "Falha ao enviar email: " + e.getMessage());
                 }
@@ -55,7 +68,7 @@ public class App {
 
         } else {
             // O usuário clicou em Cancelar ou fechou o diálogo
-            JOptionPane.showMessageDialog(null,"Cadastro de senha cancelado.");
+            JOptionPane.showMessageDialog(null, "Cadastro de senha cancelado.");
         }
     }
 }
